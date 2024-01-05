0f2d3d5bb9cbec0d0661b956f3c91deb077d1bb9
Prabhugurudarshan Singh <prabhu25.1975@gmail.com>
2024-01-05 13:31:13 +0530

    Implemented the repository.

---
+++ Movies.Infrastructure/Repositories/MovieRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.Core.Entities;
+using Movies.Core.Repositories;
+using Movies.Infrastructure.Data;
+using Movies.Infrastructure.Repositories.Base;
+
+namespace Movies.Infrastructure.Repositories;
+
+public class MovieRepository: Repository<Movie>, IMovieRepository
+{
+    
+
+    public MovieRepository(MovieContext movieContext) : base(movieContext)
+    {
+        
+    }
+
+    public async Task<IEnumerable<Movie>> GetMovieByDirectorName(string directorName)
+    {
+        return await _movieContext.Movies
+            .Where(m => m.DirectorName == directorName)
+            .ToListAsync();
+    }
+}
