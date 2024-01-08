081681430c7326145373562951834dbaa266378d
Prabhugurudarshan Singh <prabhu25.1975@gmail.com>
2024-01-08 14:45:30 +0530

    Map the Query handlers.

---
+++ Movies.Application/Commands/GetMoviesByDirectorNameHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Movies.Application.Mappers;
+using Movies.Application.Queries;
+using Movies.Application.Responses;
+using Movies.Core.Repositories;
+
+namespace Movies.Application.Commands;
+
+public class GetMoviesByDirectorNameHandler: IRequestHandler<GetMoviesByDirectorNameQuery, IEnumerable<MovieResponse>>
+{
+    private readonly IMovieRepository _movieRepository;
+
+    public GetMoviesByDirectorNameHandler(IMovieRepository movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+    public async Task<IEnumerable<MovieResponse>> Handle(GetMoviesByDirectorNameQuery request, CancellationToken cancellationToken)
+    {
+        var movies = await _movieRepository.GetMovieByDirectorName(request.DirectorName);
+        var movieResponses = MovieMapper.Mapper.Map<IEnumerable<MovieResponse>>(movies);
+        return movieResponses;
+    }
+}
